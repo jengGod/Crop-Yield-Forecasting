ff6be14688bc4a01009373025d1fceceb1786d83
jenggod <65011212012@msu.ac.th>
2025-02-03 20:26:56 +0700

    test_map_province

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from myapp import views
+
 urlpatterns = [
     path('',views.index),
-    path('model',views.model),  
+    path('model',views.model),
+    path('provinces/', views.province),  
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Province 
 # Create your views here.
 
 def index(request):
@@ -7,3 +8,7 @@ def index(request):
 
 def model(request):
     return render(request,"model.html")  
+
+def province_list(request):
+    provinces = Province.objects.all().order_by('name')  # ดึงข้อมูลจังหวัดทั้งหมดและเรียงตามชื่อ
+    return render(request, "province.html", {"provinces": provinces})    
